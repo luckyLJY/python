c09acb718a00b16e4f5b5c0db82f4ddefc110041
“luckyLJY” <1790311731@qq.com>
2022-05-18 18:40:29 +0800

    2022-5-18 python 下一阶段

---
+++ Base/other/project/ch21.2.2-2.py
@@ -0,0 +1,14 @@
+"""获得动态数据"""
+import re
+import urllib.request
+url = 'http://q.stock.sohu.com/hisHq?code=cn_600519&stat=1&order=D&period=d&callback=historySearchHandler&rt=jsonp&0.8115656498417958'
+
+req = urllib.request.Request(url)
+
+with urllib.request.urlopen(req) as response:
+    data = response.read()
+    htmlstr = data.decode('gbk')
+    print(htmlstr)
+    htmlstr = htmlstr.replace('historySearchHander(','')
+    htmlstr = htmlstr.replace(')','')
+    print('替换后的：',htmlstr)
---
+++ Base/other/project/project.md
@@ -0,0 +1,17 @@
+# 网络爬虫与抓取股票数据
+## 设计技术概述
+
+- 网络通信技术：Python中的urllib资源；
+- 多线程技术：
+- 数据交换技术：
+- Web前端技术：
+- 数据分析技术：
+- 数据存储技术：
+### 数据爬取
+
+1. 获取静态数据"https://www.nasdaq.com/news-and-insights#.UWdnJBDMhHk"
+ch21.2.2-1
+2. 获取动态数据
+ch21.2.2-2
+3. 伪装成浏览器iPhone Safari浏览器
+ch21.2.2.-3
